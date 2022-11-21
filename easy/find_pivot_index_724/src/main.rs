3604586502072a04d0eb458dd3e5f07296b24d37
1Aboka1 <zz.abulhair.zz@gmail.com>
2022-11-21 20:19:32 +0600

    find pivot change

--- easy/find_pivot_index_724/src/main.rs
+++ easy/find_pivot_index_724/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+fn pivot_index(nums: Vec<i32>) -> i32 {
+    let nums_len = nums.len();
+    
+    for i in 0..nums_len {
+        if nums[..i].iter().sum::<i32>() == nums[i + 1..nums_len].iter().sum::<i32>() {
+            return i as i32;
+        }
+    }
+
+    -1
 }
